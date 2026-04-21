42f5560ac44eb871b371523d37888f7f85afc8bc
Harryl3ivers <cofe267@outlook.com>
2026-04-21 14:44:50 +0100

    improved e2e tests

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,5 +1,6 @@
 from login_page import LoginPage
 from products_page import ProductsPage
+from checkout_page import CheckOutPage
 
 def test_full_user_flow(page):
     login = LoginPage(page)
@@ -10,11 +11,17 @@ def test_full_user_flow(page):
     products.add_first_product_to_cart()
     products.go_to_cart()
 
-    page.click("#checkout")
-    page.fill("#first-name", "John")
-    page.fill("#last-name", "Doe")
-    page.fill("#postal-code", "12345")
-    page.click("#continue")
-    page.click("#finish")
+    checkout = CheckOutPage(page)
+    checkout.start_checkout()
+    checkout.fill_checkout_info("John","Doe","12345")
+    checkout.continue_checkout()
+    checkout.finish_checkout()
 
-    assert "thank you for your order" in page.content().lower()
+    # page.click("#checkout")
+    # page.fill("#first-name", "John")
+    # page.fill("#last-name", "Doe")
+    # page.fill("#postal-code", "12345")
+    # page.click("#continue")
+    # page.click("#finish")
+
+    assert checkout.order_complete() == "thank you for your order" 
