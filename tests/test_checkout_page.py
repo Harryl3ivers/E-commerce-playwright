502f4a9fd61efd8ca3406d3c293f47d53cee69f4
Harryl3ivers <cofe267@outlook.com>
2026-05-09 17:35:03 +0100

    fixed checkout page test requires first name

--- tests/test_checkout_page.py
+++ tests/test_checkout_page.py
@@ -1,8 +1,6 @@
 from pages.checkout_page import CheckOutPage
 from pages.products_page import ProductsPage
 from pages.login_page import LoginPage
-from  conftest import login_page_auto
-from conftest import shop_flow
 from flows.shop_flow import ShopFlow
 import pytest
 from models.user import User
@@ -16,21 +14,23 @@ def test_user_can_complete_checkout(shop_flow,product):
     user = User("John","Doe","12345")
     shop_flow.complete_purchase(product,user)
     assert not shop_flow.checkout.has_error()
-    shop_flow.checkout.finish_checkout()
     assert "Thank you for your order!" in shop_flow.checkout.order_complete()
     
 
 def test_checkout_needs_first_name(shop_flow):
     user = User("","Shaw","12345")
-    shop_flow.complete_purchase("Sauce Labs Backpack",user)
-    assert not shop_flow.checkout.has_error()
-
+    shop_flow.products.add_product_by_name("Sauce Labs Backpack")
+    shop_flow.products.go_to_cart()
+    shop_flow.checkout.start_checkout()
+    shop_flow.checkout.fill_checkout_info(user)
+    shop_flow.checkout.continue_checkout()
+    assert shop_flow.checkout.has_error()
     assert "First Name is required" in shop_flow.checkout.get_error_message()
 
 def test_postal_code_required(shop_flow):
     user = User("Noah","Shaw","")
     shop_flow.complete_purchase("Sauce Labs Backpack",user)
-    assert not shop_flow.checkout.has_error()
+    assert  shop_flow.checkout.has_error()
     assert "Error: Postal Code is required" in shop_flow.checkout.get_error_message()
 
 @pytest.mark.parametrize("user",[
@@ -40,21 +40,19 @@ def test_postal_code_required(shop_flow):
 def test_checkout_multiple_users(shop_flow,user ):
     shop_flow.complete_purchase("Sauce Labs Backpack",user)
     assert not shop_flow.checkout.has_error()
-    shop_flow.checkout.finish_checkout()
     assert shop_flow.checkout.order_complete()
     
 
 def test_user_can_recover_from_checkout_error(shop_flow):
     user = User("","Shaw","12345")
     shop_flow.complete_purchase("Sauce Labs Backpack",user)
-    assert not shop_flow.checkout.has_error()
-    assert "Firts Name is required" in shop_flow.checkout.get_error_message()
+    assert  shop_flow.checkout.has_error()
+    assert "First Name is required" in shop_flow.checkout.get_error_message()
 
     user.first_name = "Noah"
     shop_flow.checkout.fill_checkout_info(user)
     shop_flow.checkout.continue_checkout()
     assert not shop_flow.checkout.has_error()
-    shop_flow.checkout.finish_checkout()
     assert "Thank you for your order!" in shop_flow.checkout.order_complete()
      
 
